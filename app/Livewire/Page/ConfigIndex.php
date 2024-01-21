ec28465577fe175ad298f3d13a5986bc50628f7c
feneme7370 <marascofederico95@gmail.com>
2024-01-21 18:41:06 -0300

    en public

--- app/Livewire/Page/ConfigIndex.php
+++ app/Livewire/Page/ConfigIndex.php
@@ -182,8 +182,7 @@ class ConfigIndex extends Component
 
             Storage::disk('public')->put('archives/images/hero/' . $filename, $image_hero->encode());
 
-            file::put('archives/images/hero/' . $filename, $image_hero->encode());
-            // $image_hero->save('archives/images/hero/'. $filename);
+            $image_hero->save( $filename);
             $this->image_hero = $filename;
         }
     }

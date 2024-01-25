8c4fbd754d6ff616521c9aad716e5b17d4b23c86
feneme7370 <marascofederico95@gmail.com>
2024-01-24 22:38:38 -0300

    build beta 1.1.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <script src="{{asset('lib\flowbite\flowbite.min.js')}}"></script>
+        
         
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -50,8 +50,7 @@
 
     @stack('modals')
     
+    <script src="{{asset('lib\flowbite\flowbite.min.js')}}"></script>
     @livewireScripts
-    {{-- <script src="{{asset('public/lib/windmill/js/focus-trap.js')}}"></script>
-    <script src="{{asset('public/lib/windmill/js/init-alpine.js')}}"></script> --}}
     </body>
 </html>

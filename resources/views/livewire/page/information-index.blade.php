943e82dad3e88c74c1bdd9fbd5844f40df15e8ec
feneme7370 <marascofederico95@gmail.com>
2024-03-15 00:29:52 -0300

    v 1.1.1 videos tutoriales

--- resources/views/livewire/page/information-index.blade.php
+++ resources/views/livewire/page/information-index.blade.php
@@ -55,4 +55,42 @@
 
 
   </x-sistem.filter.bg-input>
+
+  <x-sistem.filter.bg-input class="mx-auto flex-col w-full md:w-1/2 p-2 pb-5 gap-5">
+  
+  <p class="font-bold text-center text-gray-800 text-lg">Ver videos tutoriales</p>
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
+
+    <a target="_blank" class=" w-full flex justify-between gap-1 my-1 p-2 border border-gray-700 rounded hover:bg-gray-100 hover:shadow-md" href="https://www.youtube.com/watch?v=M-IPvuSCbBw">
+      <img class="h-32 w-32" src="{{ asset('archives/sistem/img/1. Tutorial de la demo.png') }}" alt="imagen">
+      <p>1. Tutorial de la demo</p>
+    </a>
+  
+    <a target="_blank" class=" w-full flex justify-between gap-1 my-1 p-2 border border-gray-700 rounded hover:bg-gray-100 hover:shadow-md" href="https://www.youtube.com/watch?v=l-gdZjUy4zs">
+      <img class="h-32 w-32" src="{{ asset('archives/sistem/img/2. Acceso al admin y configuracion.png') }}" alt="imagen">
+      <p>2. Acceso al admin y configuracion</p>
+    </a>
+  
+    <a target="_blank" class=" w-full flex justify-between gap-1 my-1 p-2 border border-gray-700 rounded hover:bg-gray-100 hover:shadow-md" href="https://www.youtube.com/watch?v=Tqt3u0vOQGU">
+      <img class="h-32 w-32" src="{{ asset('archives/sistem/img/3. Categorias.png') }}" alt="imagen">
+      <p>3. Categorias</p>
+    </a>
+  
+    <a target="_blank" class=" w-full flex justify-between gap-1 my-1 p-2 border border-gray-700 rounded hover:bg-gray-100 hover:shadow-md" href="https://www.youtube.com/watch?v=VMmibgjsDac">
+      <img class="h-32 w-32" src="{{ asset('archives/sistem/img/4. Etiquetas y productos.png') }}" alt="imagen">
+      <p>4. Etiquetas y productos</p>
+    </a>
+  
+    <a target="_blank" class=" w-full flex justify-between gap-1 my-1 p-2 border border-gray-700 rounded hover:bg-gray-100 hover:shadow-md" href="https://www.youtube.com/watch?v=hZd1BBSAp20">
+      <img class="h-32 w-32" src="{{ asset('archives/sistem/img/5. Productos masivos y destacados.png') }}" alt="imagen">
+      <p>5. Productos masivos y destacados</p>
+    </a>
+  
+    <a target="_blank" class=" w-full flex justify-between gap-1 my-1 p-2 border border-gray-700 rounded hover:bg-gray-100 hover:shadow-md" href="https://www.youtube.com/watch?v=cwZzhaNuI64">
+      <img class="h-32 w-32" src="{{ asset('archives/sistem/img/6. Perfil e informacion.png') }}" alt="imagen">
+      <p>6. Perfil e informacion</p>
+    </a>
+  </div>
+
+  </x-sistem.filter.bg-input>
 </div>
